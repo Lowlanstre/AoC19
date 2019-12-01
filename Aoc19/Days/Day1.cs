b594bebe112ff1b091ae383f5c0d56b86a05a70e
Megh <41010358+Lowlanstre@users.noreply.github.com>
2019-12-01 16:59:55 +0100

    Day1 solutions

---
+++ Aoc19/Days/Day1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Aoc19.Days
+
+{
+    public static class Day1
+    {
+        public static long Part1(string[] modules)
+        {
+            long totalFuel;
+            totalFuel = 0;
+            foreach (var module in modules)
+            {
+                var mass = Convert.ToInt64(module);
+                mass = mass / 3 - 2;
+                    totalFuel += mass;
+            }
+            return totalFuel;
+        }
+        public static long Part2(string[] modules)
+        {
+            long totalFuel;
+            totalFuel = 0;
+            foreach (var module in modules)
+            {
+                var mass = Convert.ToInt64(module);
+                while (mass/3 - 2 > 0)
+                {
+                    mass = mass / 3 - 2;
+                    totalFuel += mass;
+                }
+            }
+            return totalFuel;
+        }
+    }
+}
---
+++ Aoc19/Daytest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Aoc19
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(Days.Day1.Part1(args));
+            Console.WriteLine(Days.Day1.Part2(args));
+        }
+    }
+}
