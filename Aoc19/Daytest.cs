6f78318424c5b18f3e1c039a886738a66b3f3f04
Megh <41010358+Lowlanstre@users.noreply.github.com>
2019-12-01 17:03:52 +0100

    Useless comments

--- Aoc19/Daytest.cs
+++ Aoc19/Daytest.cs
@@ -4,10 +4,15 @@ namespace Aoc19
 {
     internal class Program
     {
-        public static void Main(string[] args)
+        public static void Main(string[] args) // Puzzle input goes into program arguments
         {
-            Console.WriteLine(Days.Day1.Part1(args));
-            Console.WriteLine(Days.Day1.Part2(args));
+            /*
+             * Example of testing solutions    
+             * 
+             * Console.WriteLine(Days.Day1.Part1(args));
+             * Console.WriteLine(Days.Day1.Part2(args));
+             */
+
         }
     }
 }
